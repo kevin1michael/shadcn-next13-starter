e27275692e87bd30369b591f01b30da7a3dcf41a
Paarth Madan <paarth.mdn@gmail.com>
2023-07-17 13:44:57 -0400

    Export Toaster

--- components/ui/toaster.tsx
+++ components/ui/toaster.tsx
@@ -8,7 +8,7 @@ import {
 } from "../../components/ui/toast"
 import { useToast } from "../../components/ui/use-toast"
 
-export function Toaster() {
+function Toaster() {
   const { toasts } = useToast()
 
   return (
@@ -31,3 +31,5 @@ export function Toaster() {
     </ToastProvider>
   )
 }
+
+export { Toaster };
