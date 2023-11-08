229189940fa902d90788ebe230e4562abee1cde4
petergok <petergok@hotmail.com>
2023-11-08 11:50:47 -0500

    Pushed updates, bumped tempo-devtools

--- next.config.js
+++ next.config.js
@@ -6,4 +6,10 @@ const nextConfig = {
   },
 };
 
+if (process.env.NEXT_PUBLIC_TEMPO) {
+  nextConfig["experimental"] = {
+    swcPlugins: [[require.resolve("@petergok/tempo-devtools/swc"), {}]],
+  };
+}
+
 module.exports = nextConfig;
