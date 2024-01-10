6c72e32682fbd79b2e38788d0824c97b32f49e6f
“Kevin <kevin.michael.905@gmail.com>
2024-01-10 10:16:23 -0500

    Updated dev tools imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { TempoDevtools } from "@petergok/tempo-devtools";
-TempoDevtools.init();
+import { useEffect } from "react";
+import { TempoDevtools } from "tempo-devtools";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if (process.env.NEXT_PUBLIC_TEMPO) {
+      TempoDevtools.init();
+    }
+  }, []);
   return <Component {...pageProps} />;
 }
